766601a604cf6ae9bb5bee150da0f3f32974302d
BryanM804 <bmulholland41@gmail.com>
2024-01-17 16:09:56 -0500

    Converted to autocomplete

--- src/index.js
+++ src/index.js
@@ -16,17 +16,35 @@ client.on("ready", (c) => {
 //Interaction is the event triggered when someone uses a command
 client.on("interactionCreate", (interaction) =>{
     
-    if(interaction.isAutocomplete()){
-        console.log("Autocomplete interaction");
-        (async () =>{
-            interaction = AutocompleteInteraction(interaction);
-            const focused = interaction.options.getFocused().value;
-            const choices = ["Barbell Bench", "Dumbbell Bench"];
-            const filtered = choices.filter(choice => choice.startsWith(focused));
-            await interaction.respond(
-                filtered.map(choice => ({name: choice, value: choice})),
-            );
-        })
+    if(interaction.isAutocomplete() && interaction.commandName === "log"){
+        const focused = interaction.options.getFocused();
+        const choices = [
+            "Barbell Bench Press", "Dumbbell Bench Press", "Chest Press Machine",
+            "Incline Barbell Bench Press", "Incline Dumbbell Bench Press", "Incline Chest Press Machine",
+            "Decline Barbell Bench Press", "Decline Dumbbell Bench Press", "Decline Chest Press Machine",
+            "Dip", "Assissted Dip", "Barbell Shoulder Press/Military Press",
+            "Dumbbell Shoulder Press", "Arnold Press", "Machine Shoulder Press",
+            "Lateral Raise", "Cable Lateral Raise", "Machine Lateral Raise",
+            "Front Cable Raise", "Front Dumbbell Raise", "Pull Up", "Chin Up",
+            "Assissted Pullup", "Lat Pulldown", "Lat Pullover", "Single Arm Pulldown",
+            "Barbell Row", "T Bar Row", "Landmine", "Machine Row",
+            "Machine Pulldown", "Cable Row", "Face Pull",
+            "Rear Delt Fly", "Rear Delt Ski", "Romanian Deadlift",
+            "Bulgarian Split Squat", "Good Morning", "Lunge",
+            "Reverse Lunge", "Hip Abduction", "Hip Adduction",
+            "Calf Raise", "Cable Crunch", "Goblett Squat",
+            "Barbell Squat", "Leg Extension", "Hamstring Curl",
+            "Lying Hamstring Curl", "Deadlift", "Tricep Pulldown",
+            "Overhead Tricep Extension", "Tricep Pushdown", "Single Arm Tricep Extension",
+            "Tricep Machine", "Dumbbell Bicep Curl", "Hammer Curl", "Zottman Curl",
+            "Barbell Bicep Curl", "Preacher Curl", "Bicep Curl Machine", "Preacher Curl Machine",
+            "Reverse Curl"
+    
+        ];
+        const filtered = choices.filter(choice => choice.toLowerCase().indexOf(focused.toLowerCase()) >= 0);
+        interaction.respond(
+            filtered.slice(0,24).map(choice => ({name: choice, value: choice}))
+        );
     }
 
     if(!interaction.isChatInputCommand()) return;
