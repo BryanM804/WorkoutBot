ea2a45ddc6f48b11b6f39304bc332813e49bc4fd
BryanM804 <bmulholland41@gmail.com>
2024-02-27 18:49:47 -0500

    actually fixed this time

--- src/events/ready/setActivity.js
+++ src/events/ready/setActivity.js
@@ -1,45 +1,52 @@
 const { ActivityType } = require("discord.js");
 
+let botClient;
+
 module.exports = (client) => {
-    // This is to get it to run once at startup then every half hour from there.
-    let intervalTime = 1;
+    botClient = client;
+    setRandomActivity();
+}
 
-    setInterval(() => {
-        let random = Math.floor((Math.random() * 4) + 1)
-        
-        switch (random) {
-            case 1:
-                client.user.setActivity({
-                    name: "with heavy circles",
-                    type: ActivityType.Playing
-                });
-                break;
-            case 2:
-                client.user.setActivity({
-                    name: "21 Pump Street",
-                    type: ActivityType.Watching
-                });
-                break;
-            case 3:
-                client.user.setActivity({
-                    name: "the posedown",
-                    type: ActivityType.Watching
-                });
-                break;
-            case 4:
-                client.user.setActivity({
-                    name: "Clash Royale on the recumbent bike",
-                    type: ActivityType.Playing
-                });
-                break;
-            default:
-                client.user.setActivity({
-                    name: "with heavy circles",
-                    type: ActivityType.Playing
-                });
-                break;  
-        }
-
-        intervalTime = 1800000;// Every half an hour
-    }, intervalTime);
+function setRandomActivity() {
+    let random = Math.floor((Math.random() * 5) + 1)
+    
+    switch (random) {
+        case 1:
+            botClient.user.setActivity({
+                name: "with heavy circles",
+                type: ActivityType.Playing
+            });
+            break;
+        case 2:
+            botClient.user.setActivity({
+                name: "21 Pump Street",
+                type: ActivityType.Watching
+            });
+            break;
+        case 3:
+            botClient.user.setActivity({
+                name: "the posedown",
+                type: ActivityType.Watching
+            });
+            break;
+        case 4:
+            botClient.user.setActivity({
+                name: "Clash Royale on the recumbent bike",
+                type: ActivityType.Playing
+            });
+            break;
+        case 5:
+            botClient.user.setActivity({
+                name: "Back to Me by Kanye West",
+                type: ActivityType.Listening
+            });
+            break;
+        default:
+            botClient.user.setActivity({
+                name: "with heavy circles",
+                type: ActivityType.Playing
+            });
+            break;  
+    }
+    setTimeout(setRandomActivity, 1800000); // Every 30 minutes
 }
