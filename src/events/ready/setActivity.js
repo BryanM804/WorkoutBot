ff3f65e59f6203a3b949808d72e195a9d3f72eb5
BryanM804 <bmulholland41@gmail.com>
2024-02-27 16:35:30 -0500

    added activities

--- src/events/ready/setActivity.js
+++ src/events/ready/setActivity.js
@@ -1,7 +1,39 @@
 const { ActivityType } = require("discord.js");
+
 module.exports = (client) => {
-    client.user.setActivity({
-        name: "with heavy circles",
-        type: ActivityType.Playing
-    });
+    let random = (int)(Math.random() * 4) + 1;
+    
+    switch (random) {
+        case 1:
+            client.user.setActivity({
+                name: "with heavy circles",
+                type: ActivityType.Playing
+            });
+            break;
+        case 2:
+            client.user.setActivity({
+                name: "21 Pump Street",
+                type: ActivityType.Watching
+            });
+            break;
+        case 3:
+            client.user.setActivity({
+                name: "the posedown",
+                type: ActivityType.Watching
+            });
+            break;
+        case 4:
+            client.user.setActivity({
+                name: "Clash Royale on the recumbent bike",
+                type: ActivityType.Playing
+            });
+            break;
+        default:
+            client.user.setActivity({
+                name: "with heavy circles",
+                type: ActivityType.Playing
+            });
+            break;  
+    }
+    setTimeout(this, 900000);
 }
